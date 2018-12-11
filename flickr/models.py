436024c4c19e61789a93e03e0ea5b7c5e1e24470
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-11 12:20:02 +0300

    add location model

--- flickr/models.py
+++ flickr/models.py
@@ -12,3 +12,15 @@ class categories(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
