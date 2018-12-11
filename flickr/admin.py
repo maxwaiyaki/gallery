65eed6a2ff641ca84af6393906c0991d8c0254a5
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-11 11:54:41 +0300

    register models and add horizontal filter for image categories in admin.py

--- flickr/admin.py
+++ flickr/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
+    
+admin.site.register(Location)
+admin.site.register(Image,ImageAdmin)
