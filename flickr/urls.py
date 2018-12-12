7bbbb1605cec88c3e484022fa81b37a2035deac9
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-12 18:29:44 +0300

    add template files

--- flickr/urls.py
+++ flickr/urls.py
@@ -3,8 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns[
+urlpatterns = [
     url('^$',views.main_gallery,name = 'main_gallery'),
     url(r'^location/(\d+)',views.location,name = 'location'),
     url(r'^search/',views.search,name='search')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
