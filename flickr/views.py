d706481ab11736f0cce2018baaf6d2c84686e24d
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-12 15:16:11 +0300

    add main and location views

--- flickr/views.py
+++ flickr/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Image,Location
 
-# Create your views here.
+def main_gallery(request):
+    images = Image.all_images()
+    locations = Location.objects.all()
+    return render(request, 'index.html', {"images":images,"locations":locations})
+
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"locations":locations,"images":images})
