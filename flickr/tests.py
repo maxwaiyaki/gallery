2b8c9ada9d55627ddfdce2d42700338d27d843bb
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-12 15:12:28 +0300

    add save location method

--- flickr/tests.py
+++ flickr/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+#testing the save method
+def save_save_method(self):
+    self.james.save_location()
+    self.nairobi.save_location()
+    self.assertTrue(len(locations) > 0)
